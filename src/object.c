ce8ea0fec5c9ede3620e6eea58190ab0824600ea
Igor Wiedler <igor@igor.io>
2016-11-28 02:23:16 +0100

    remove a bunch of unused commands

--- src/object.c
+++ src/object.c
@@ -370,11 +370,6 @@ int compareStringObjects(robj *a, robj *b) {
     return compareStringObjectsWithFlags(a,b,REDIS_COMPARE_BINARY);
 }
 
-/* Wrapper for compareStringObjectsWithFlags() using collation. */
-int collateStringObjects(robj *a, robj *b) {
-    return compareStringObjectsWithFlags(a,b,REDIS_COMPARE_COLL);
-}
-
 /* Equal string objects return 1 if the two objects are the same from the
  * point of view of a string comparison, otherwise 0 is returned. Note that
  * this function is faster then checking for (compareStringObject(a,b) == 0)
@@ -388,57 +383,6 @@ int equalStringObjects(robj *a, robj *b) {
     } else {
         return compareStringObjects(a,b) == 0;
     }
-}
-
-size_t stringObjectLen(robj *o) {
-    serverAssertWithInfo(NULL,o,o->type == OBJ_STRING);
-    if (sdsEncodedObject(o)) {
-        return sdslen(o->ptr);
-    } else {
-        return sdigits10((long)o->ptr);
-    }
-}
-
-int getDoubleFromObject(const robj *o, double *target) {
-    double value;
-    char *eptr;
-
-    if (o == NULL) {
-        value = 0;
-    } else {
-        serverAssertWithInfo(NULL,o,o->type == OBJ_STRING);
-        if (sdsEncodedObject(o)) {
-            errno = 0;
-            value = strtod(o->ptr, &eptr);
-            if (isspace(((const char*)o->ptr)[0]) ||
-                eptr[0] != '\0' ||
-                (errno == ERANGE &&
-                    (value == HUGE_VAL || value == -HUGE_VAL || value == 0)) ||
-                errno == EINVAL ||
-                isnan(value))
-                return C_ERR;
-        } else if (o->encoding == OBJ_ENCODING_INT) {
-            value = (long)o->ptr;
-        } else {
-            serverPanic("Unknown string encoding");
-        }
-    }
-    *target = value;
-    return C_OK;
-}
-
-int getDoubleFromObjectOrReply(client *c, robj *o, double *target, const char *msg) {
-    double value;
-    if (getDoubleFromObject(o, &value) != C_OK) {
-        if (msg != NULL) {
-            addReplyError(c,(char*)msg);
-        } else {
-            addReplyError(c,"value is not a valid float");
-        }
-        return C_ERR;
-    }
-    *target = value;
-    return C_OK;
 }
 
 int getLongDoubleFromObject(robj *o, long double *target) {
@@ -460,20 +404,6 @@ int getLongDoubleFromObject(robj *o, long double *target) {
         } else {
             serverPanic("Unknown string encoding");
         }
-    }
-    *target = value;
-    return C_OK;
-}
-
-int getLongDoubleFromObjectOrReply(client *c, robj *o, long double *target, const char *msg) {
-    long double value;
-    if (getLongDoubleFromObject(o, &value) != C_OK) {
-        if (msg != NULL) {
-            addReplyError(c,(char*)msg);
-        } else {
-            addReplyError(c,"value is not a valid float");
-        }
-        return C_ERR;
     }
     *target = value;
     return C_OK;
@@ -526,64 +456,4 @@ int getLongFromObjectOrReply(client *c, robj *o, long *target, const char *msg) 
     }
     *target = value;
     return C_OK;
-}
-
-char *strEncoding(int encoding) {
-    switch(encoding) {
-    case OBJ_ENCODING_RAW: return "raw";
-    case OBJ_ENCODING_INT: return "int";
-    case OBJ_ENCODING_HT: return "hashtable";
-    case OBJ_ENCODING_QUICKLIST: return "quicklist";
-    case OBJ_ENCODING_ZIPLIST: return "ziplist";
-    case OBJ_ENCODING_INTSET: return "intset";
-    case OBJ_ENCODING_SKIPLIST: return "skiplist";
-    case OBJ_ENCODING_EMBSTR: return "embstr";
-    default: return "unknown";
-    }
-}
-
-/* =========================== Memory introspection ========================== */
-
-/* Helper for "MEMORY allocator-stats", used as a callback for the jemalloc
- * stats output. */
-void inputCatSds(void *result, const char *str) {
-    /* result is actually a (sds *), so re-cast it here */
-    sds *info = (sds *)result;
-    *info = sdscat(*info, str);
-}
-
-/* ======================= The OBJECT and MEMORY commands =================== */
-
-/* This is a helper function for the OBJECT command. We need to lookup keys
- * without any modification of LRU or other parameters. */
-robj *objectCommandLookup(client *c, robj *key) {
-    dictEntry *de;
-
-    if ((de = dictFind(c->db->dict,key->ptr)) == NULL) return NULL;
-    return (robj*) dictGetVal(de);
-}
-
-robj *objectCommandLookupOrReply(client *c, robj *key, robj *reply) {
-    robj *o = objectCommandLookup(c,key);
-
-    if (!o) addReply(c, reply);
-    return o;
-}
-
-/* Object command allows to inspect the internals of an Redis Object.
- * Usage: OBJECT <refcount|encoding|idletime> <key> */
-void objectCommand(client *c) {
-    robj *o;
-
-    if (!strcasecmp(c->argv[1]->ptr,"refcount") && c->argc == 3) {
-        if ((o = objectCommandLookupOrReply(c,c->argv[2],shared.nullbulk))
-                == NULL) return;
-        addReplyLongLong(c,o->refcount);
-    } else if (!strcasecmp(c->argv[1]->ptr,"encoding") && c->argc == 3) {
-        if ((o = objectCommandLookupOrReply(c,c->argv[2],shared.nullbulk))
-                == NULL) return;
-        addReplyBulkCString(c,strEncoding(o->encoding));
-    } else {
-        addReplyError(c,"Syntax error. Try OBJECT (refcount|encoding|idletime|freq)");
-    }
 }
